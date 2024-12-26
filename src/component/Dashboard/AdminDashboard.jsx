8a874b0b8b967b47f7b05be6a64f717a8e5e3410
Mohammad <mohd.umair2325@gmail.com>
2024-12-27 02:31:26 +0530

    Name and button update

--- src/component/Dashboard/AdminDashboard.jsx
+++ src/component/Dashboard/AdminDashboard.jsx
@@ -5,7 +5,7 @@ import AllTask from "../TaskList/AllTask";
 
 const AdminDashboard = ({logout, data, setData}) => {
   return (
-    <div className="p-5 px-10 bg-[#1C1C1C] h-[1240px] md:h-screen">
+    <div className="p-5 px-10 bg-[#1C1C1C] h-[1240px] md:h-screen text-white">
       <Header logout={logout} data={data}/>
       <CreateTask />
       <AllTask />
--- src/component/Dashboard/EmpDashboard.jsx
+++ src/component/Dashboard/EmpDashboard.jsx
@@ -5,7 +5,7 @@ import TaskList from "../TaskList/TaskList";
 
 const EmpDashboard = ({ logout, data, setData }) => {
   return (
-    <div className="p-10 bg-[#1C1C1C] h-full md:h-screen">
+    <div className="p-10 bg-[#1C1C1C] h-full md:h-screen text-white">
       <Header logout={logout} data={data} />
       {data && (
         <>
