3769a34876c54dc09cae78a36a17df61a8d8e49b
mdumair0 <mohd.umair2325@gmail.com>
2026-01-18 23:07:29 +0530

    cors issue fix

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URL, {
+  ssl: true,
+  tls: true,
+  tlsAllowInvalidCertificates: false,
+  serverSelectionTimeoutMS: 5000
 });
